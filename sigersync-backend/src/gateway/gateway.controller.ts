cb4bb5f926fa8cc0a642e0c5aed319f38f3f8a3a
Tanitech <lorensiusfirngadi78@guru.sd.belajar.id>
2026-05-10 16:53:20 +0700

    Membuat Gateway Controller ts

---
+++ sigersync-backend/src/gateway/gateway.controller.ts
@@ -0,0 +1,25 @@
+import { Controller, Get, HttpCode, HttpStatus } from '@nestjs/common';
+import { GatewayService } from './gateway.service';
+
+@Controller()
+export class GatewayController {
+  constructor(private readonly gatewayService: GatewayService) {}
+
+  @Get()
+  @HttpCode(HttpStatus.OK)
+  getHealth() {
+    return this.gatewayService.getHealth();
+  }
+
+  @Get('services')
+  @HttpCode(HttpStatus.OK)
+  getServices() {
+    return this.gatewayService.getServices();
+  }
+
+  @Get('health')
+  @HttpCode(HttpStatus.OK)
+  health() {
+    return this.gatewayService.getHealth();
+  }
+}
