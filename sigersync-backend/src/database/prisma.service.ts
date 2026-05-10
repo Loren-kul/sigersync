96c376b91f5d23e8cc531dbe193dbb43f65b6041
Tanitech <lorensiusfirngadi78@guru.sd.belajar.id>
2026-05-10 16:37:05 +0700

    Setup database Module pada folder src

---
+++ sigersync-backend/src/database/prisma.service.ts
@@ -0,0 +1,16 @@
+import { Injectable, OnModuleInit, OnModuleDestroy } from '@nestjs/common';
+import { PrismaClient } from '@prisma/client';
+
+@Injectable() // Menandai bahwa Layanan yang bisa digunakan oleh bagian aplikasi lain
+export class PrismaService extends PrismaClient implements OnModuleInit, OnModuleDestroy {
+  
+  // Fungsi yang otomatis berjalan saat aplikasi Pertama kali menyala
+  async onModuleInit() {
+    await this.$connect(); // Perintah untuk membuka koneksi ke database
+  }
+
+  // Fungsi yang otomatis berjalan saat aplikasi di matikan
+  async onModuleDestroy() {
+    await this.$disconnect(); // Perintah untuk memutus koneksi agar database tidak terbebani
+  }
+}
