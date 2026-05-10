544fe427b522d12a1f637dbcbe3a2aa50e8688a6
Tanitech <lorensiusfirngadi78@guru.sd.belajar.id>
2026-05-10 19:26:01 +0700

    Membuat folder dto dan file create-kategori.dto.ts

---
+++ sigersync-backend/src/kategori/dto/create-kategori.dto.ts
@@ -0,0 +1,24 @@
+import { IsString, IsOptional, IsNotEmpty, MinLength, MaxLength } from 'class-validator';
+
+export class CreateKategoriDTO {
+  @IsString()
+  @IsNotEmpty()
+  @MinLength(2)
+  @MaxLength(100)
+  name: string;
+
+  @IsString()
+  @IsNotEmpty()
+  @MinLength(2)
+  @MaxLength(100)
+  slug: string;
+
+  @IsString()
+  @IsOptional()
+  @MaxLength(500)
+  description?: string;
+
+  @IsString()
+  @IsOptional()
+  icon?: string;
+}
