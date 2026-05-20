914583d72a95ac19f2cb1f4f8d35c01507f3aa9f
Tanitech <lorensiusfirngadi78@guru.sd.belajar.id>
2026-05-20 22:33:53 +0700

    Membuat file app.service.ts pada folder src

---
+++ sigersync-backend/src/app.service.ts
@@ -0,0 +1,13 @@
+import { Injectable } from '@nestjs/common';
+
+// @Injectable() menandakan bahwa kelas ini adalah 'pelayan' (service)
+// yang datanya bisa digunakan oleh bagian lain aplikasi.
+@Injectable()
+export class AppService {
+  
+  // Fungsi ini bertugas menyiapkan teks sapaan.
+  getHello(): string {
+    // Memberikan jawaban berupa tulisan selamat datang.
+    return 'Welcome to SigerSync Backend Microservices API!';
+  }
+}
