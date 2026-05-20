37a2e68375260cebb23509147fcf89ce6c77769b
Tanitech <lorensiusfirngadi78@guru.sd.belajar.id>
2026-05-20 23:32:51 +0700

    Perbaikan kode pada file kategori.entity.ts di src/service/kategori

--- sigersync-backend/src/services/kategori/entities/kategori.entity.ts
+++ sigersync-backend/src/services/kategori/entities/kategori.entity.ts
@@ -1,11 +1,11 @@
 export class KategoriEntity {
-  id: string;
-  name: string;
-  slug: string;
-  description?: string;
-  icon?: string;
-  createdAt: Date;
-  updatedAt: Date;
+  id!: string;
+  name!: string;
+  slug!: string;
+  description?: string | null;
+  icon?: string | null;
+  createdAt!: Date;
+  updatedAt!: Date;
 
   constructor(data: Partial<KategoriEntity>) {
     Object.assign(this, data);
